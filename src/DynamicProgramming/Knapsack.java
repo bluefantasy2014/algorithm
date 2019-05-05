7f9de39accf268a82811b6609776503059c0b9aa
jijunshi <jijunshi@creditease.cn>
2019-05-05 16:26:28 +0800

    TBC： 未完待续

---
+++ src/DynamicProgramming/Knapsack.java
@@ -0,0 +1,25 @@
+package DynamicProgramming;
+
+/*
+* 0-1 背包问题
+*
+*
+*
+*
+* */
+public class Knapsack {
+    public static void main(String[] args) {
+        int[] a = new int[]{2,10,2,6}; //物品体积
+        int M = 10; //背包容量
+    }
+
+//TODO
+//    public static int knapSack(int [] a, int M){
+//        //创建一个而为数组，行数为物品的个数； 列数为背包容量M；
+//        int[][] s = new int[a.length][M];
+//
+//
+//    }
+
+
+}
